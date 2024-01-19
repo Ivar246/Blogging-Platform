31e16968621c64a905b76842dbedcdf642c3dba2
Ravi Shrestha <ravistha869@gmail.com>
2024-01-19 13:50:40 +0545

    create error handler route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,11 +1,12 @@
 import User from "../models/User.js";
 import * as argon from "argon2"
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === "" || email === "" || password === "")
-        return res.status(400).json({ message: "All Fields are required" })
+        return next(errorHandler(400, "All fields are required"))
 
     const hash = argon.hash(password)
     const newUser = new User({
@@ -17,6 +18,6 @@ export const signup = async (req, res) => {
         await newUser.save()
         return res.json({ message: "Signup successfull" });
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        return next(error)
     }
 }
--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,17 @@ const PORT = process.env.PORT
 
 
 app.use("/api/user", userRoutes);
-
 app.use('/api/auth', authRoutes)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
 
 app.listen(PORT, () => console.log(`server is running in the port ${PORT}`))
 
