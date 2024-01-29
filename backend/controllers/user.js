95301d1cbf272c68a65f80776633cd5b9a29333a
Ravi Shrestha <ravistha869@gmail.com>
2024-01-29 15:36:24 +0545

    fix logical error in signin functionality

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,16 +20,16 @@ export const updateUser = async (req, res, next) => {
     if (req.body.username) {
         if (req.body.username.length < 7 || req.body.username.length > 20)
             return next(errorHandler(400, "Username must be between 7 and 20 characters."))
+
+        if (req.body.username.includes(" "))
+            return next(errorHandler(400, "Username cannot contain spaces."));
+
+        if (req.body.username !== req.body.username?.toLowerCase())
+            return next(errorHandler(400, "Username must be lowercase."));
+
+        if (!req.body.username.match(/^[a-zA-z0-9]+$/))
+            return next(errorHandler(400, "Username only contain letters and numbers."))
     }
-
-    if (req.body.username.includes(" "))
-        return next(errorHandler(400, "Username cannot contain spaces."));
-
-    if (req.body.username !== req.body.username.toLowerCase())
-        return next(errorHandler(400, "Username must be lowercase."));
-
-    if (!req.body.username.match(/^[a-zA-z0-9]+$/))
-        return next(errorHandler(400, "Username only contain letters and numbers."))
 
     try {
         const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
@@ -45,6 +45,7 @@ export const updateUser = async (req, res, next) => {
         return res.status(200).json(rest)
 
     } catch (error) {
+        console.log("hi error")
         next(error)
     }
 }
