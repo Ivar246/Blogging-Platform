fa263b784acdea60d7827081f982d977bbcdd78f
Ravi Shrestha <ravistha869@gmail.com>
2024-01-31 13:26:41 +0545

    add signout route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, updateUser, deleteUser } from "../controllers/user.js"
+import { test, updateUser, deleteUser, signout } from "../controllers/user.js"
 import { authenticate } from "../utils/verifyUser.js";
 const router = express.Router()
 
 router.get("/test", test);
 router.put("/update/:userId", authenticate, updateUser);
 router.delete("/delete/:userId", authenticate, deleteUser);
+router.post("/signout", signout)
 
 export default router
