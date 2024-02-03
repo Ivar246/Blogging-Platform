d9a79a8d1ab2db91265bb3e8222143602009fc56
Ravi Shrestha <ravistha869@gmail.com>
2024-02-03 13:50:36 +0545

    add get posts api route

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -2,7 +2,6 @@ import { errorHandler } from "../utils/error.js"
 import Post from "../models/Post.js";
 
 export const createPost = async (req, res, next) => {
-    console.log()
     if (!req.user.isAdmin) {
         return next(errorHandler(403, "You are not allowed to create a post."));
     }
@@ -26,5 +25,48 @@ export const createPost = async (req, res, next) => {
         res.status(201).json(savedPost)
     } catch (error) {
         next(error)
+    }
+}
+
+export const getPosts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === 'asc' ? 1 : -1;
+
+        const posts = await Post.find({
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { category: req.query.category }),
+            ...(req.query.slug && { slug: req.query.slug }),
+            ...(req.query.postId && { _id: req.query.postId }),
+            ...(req.query.searchTerms && {
+                $or: [
+                    { title: { $regex: req.query.searchTerms, $options: "i" } },
+                    { content: { $regex: req.query.searchTerms, $options: "i" } }
+                ]
+            })
+        }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
+
+        const totalPosts = await Post.countDocuments();
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: {
+                $gte: oneMonthAgo
+            }
+        });
+
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMonthPosts,
+        });
+    } catch (error) {
+        next(error);
     }
 }
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createPost } from '../controllers/post.js';
+import { createPost, getPosts } from '../controllers/post.js';
 import { authenticate } from '../utils/verifyUser.js'
 
 
 const router = express.Router()
 
-router.post('/create', authenticate, createPost)
+router.post('/create', authenticate, createPost);
+router.get("/getPosts", getPosts);
 
 export default router;
