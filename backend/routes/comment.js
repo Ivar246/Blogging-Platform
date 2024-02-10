c497900ef8421b69630626afd47109ef752a674a
Ravi Shrestha <ravistha869@gmail.com>
2024-02-10 14:28:07 +0545

    add like comment route

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createComment, getPostComments } from "../controllers/Comment.js";
+import { createComment, getPostComments, likeComment } from "../controllers/Comment.js";
 import { authenticate } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", authenticate, createComment);
 router.get("/getPostComments/:postId", getPostComments);
+router.put("/likeComment/:commendId", authenticate, likeComment);
 
 export default router;
