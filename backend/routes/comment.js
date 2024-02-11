643524f40ecde88e3a115d1a5ec267728fe4944d
Ravi Shrestha <ravistha869@gmail.com>
2024-02-11 23:20:34 +0545

    add edit comment route

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createComment, getPostComments, likeComment } from "../controllers/Comment.js";
+import { createComment, getPostComments, likeComment, editComment } from "../controllers/Comment.js";
 import { authenticate } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post("/create", authenticate, createComment);
 router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", authenticate, likeComment);
+router.put("/editComment/:commentId", authenticate, editComment);
+
 
 export default router;
