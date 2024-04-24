29abd6d433cb39ec58db004741ef5ae33f8ac204
Ravi Shrestha <ravistha869@gmail.com>
2024-04-24 15:58:44 +0545

    complete about and project page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>About</div>
+        <div className='min-h-screen flex items-center justify-center'>
+            <div className="max-w-2xl mx-auto p-3">
+                <div>
+                    <h1 className='text-3xl font-semibold dark:text-white text-center my-7'>About Ravi's Blog</h1>
+                    <div className='flex flex-col gap-6 text-gray-500 text-md'>
+                        <p className='text-center'>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Sapiente mollitia saepe velit vero reiciendis ut perferendis quam obcaecati vitae? Sunt reiciendis perspiciatis accusantium autem rerum atque aut soluta, dolorem cumque.
+                        </p>
+                        <p className='text-center'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempora amet autem optio mollitia placeat,
+                            reiciendis quibusdam provident molestias laborum nam cupiditate maxime ab temporibus facilis explicabo, iusto excepturi earum.
+                        </p>
+                        <p className='text-center'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod impedit obcaecati ullam consectetur, repudiandae nam enim porro libero architecto fugit incidunt tempora tempore expedita alias,
+                            numquam nulla perspiciatis atque optio.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import CallToAction from "../components/CallToAction"
 
 export default function Projects() {
     return (
-        <div>Projects</div>
+        <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+            <h1 className='text-3xl font-semibold text-white'>Projects</h1>
+            <p className='text-md text-gray-500'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, similique. Quisquam illo adipisci aperiam laboriosam veniam nisi velit accusantium, laudantium eligendi ad.
+                Natus, ratione labore! Cupiditate repellat velit aliquid quidem.</p>
+            <CallToAction />
+        </div>
     )
 }
